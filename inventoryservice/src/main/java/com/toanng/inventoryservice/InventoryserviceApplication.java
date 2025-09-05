a802621610576a3632bf5421bb1c31fa0a663576
Toan Nguyen <codehub.personal@gmail.com>
2025-09-05 17:53:04 +0700

    test-update

--- inventoryservice/src/main/java/com/toanng/inventoryservice/InventoryserviceApplication.java
+++ inventoryservice/src/main/java/com/toanng/inventoryservice/InventoryserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventoryserviceApplication {
 
 	public static void main(String[] args) {
+		//@test
 		SpringApplication.run(InventoryserviceApplication.class, args);
 	}
 
